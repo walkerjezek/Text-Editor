5c667dcc133abaed7466fd0d01fe921004817d49
walkerj <wpjezek@gmail.com>
2022-07-08 16:05:22 -0600

    database fix

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,22 +16,12 @@ const initdb = async () =>
 // Need to be updated to PUT method *************************
 export const putDb = async (content) => {
   console.log('Put to the database');
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
-
-  // Open up the desired object store.
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-
-  // Use the .put() method to update the data
   const request = store.put({ id: 1, value: content });
-
-  // Get confirmation of the request.
   const result = await request;
-  console.log("'🚀 - data saved to the database'", result);
+  // console.log("'🚀 - data saved to the database'", result);
 
 
   // console.error('putDb not implemented')
@@ -39,24 +29,13 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
   console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
-
-  // Open up the desired object store.
+  const jateDb = await openDB('contact', 1);
+  const tx = jateDb.transaction('contact', 'readonly');
   const store = tx.objectStore('contact');
-
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
-
-  // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
+  // console.log('result.value', result);
   return result;
   
   // console.error('getDb not implemented')
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,9 +30,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just another text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
