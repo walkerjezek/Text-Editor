8a15b430e6730ef16843e311a2f56eee94535ddf
walkerj <wpjezek@gmail.com>
2022-07-09 14:17:16 -0600

    fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -30,10 +30,10 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-  const jateDb = await openDB('contact', 1);
-  const tx = jateDb.transaction('contact', 'readonly');
-  const store = tx.objectStore('contact');
-  const request = store.getAll();
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
   // console.log('result.value', result);
   return result;
